3288a1a6716a4b2d087e1d12e202c1e8d6a2db41
Neon-jeff <omaksteejay@gmail.com>
2022-10-27 21:25:36 +0100

    Register Models

--- musicapp/models.py
+++ musicapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Artiste(models.Model):
     first_name=models.CharField(max_length=200)
     last_name=models.CharField(max_length=200)
-    age=models.IntegerField(max_lenght=50)
+    age=models.IntegerField()
 
     def __str__(self) :
         return self.first_name
@@ -20,5 +20,5 @@ class Song(models.Model):
          return self.title
 
 class Lyric(models.Model):
-    content=models.CharField(_MAX_LENGTH=500)
+    content=models.CharField(max_length=500)
     song_id=models.ForeignKey(Song,on_delete=models.CASCADE)
